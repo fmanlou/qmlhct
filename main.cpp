89f49428c574dc2d289af32fa6af1b184760f34e
fmanlou <2632335513@qq.com>
2024-03-30 15:23:34 +0800

    增加Htc到Rgb的颜色转换

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include "cpp/cam/hct.h"
+#include "cpp/utils/utils.h"
+
+class Hct : public QObject {
+  Q_OBJECT
+
+  Q_PROPERTY(int h READ h WRITE setH NOTIFY hChanged)
+  Q_PROPERTY(int c READ c WRITE setC NOTIFY cChanged)
+  Q_PROPERTY(int t READ t WRITE setT NOTIFY tChanged)
+  Q_PROPERTY(QString rgb READ rgb NOTIFY rgbChanged)
+
+ public:
+  Hct(QObject* parent = nullptr) : QObject(parent) {
+    connect(this, &Hct::hChanged, this, &Hct::rgbChanged);
+    connect(this, &Hct::cChanged, this, &Hct::rgbChanged);
+    connect(this, &Hct::tChanged, this, &Hct::rgbChanged);
+  }
+
+  int h() const { return hue; }
+  void setH(int h) {
+    if (h != hue) {
+      hue = h;
+      emit hChanged();
+    }
+  }
+
+  int c() const { return chroma; }
+  void setC(int c) {
+    if (c != chroma) {
+      chroma = c;
+      emit cChanged();
+    }
+  }
+
+  int t() const { return tone; }
+  void setT(int t) {
+    if (t != tone) {
+      tone = t;
+      emit tChanged();
+    }
+  }
+
+  QString rgb() const {
+    material_color_utilities::Hct d{(double)hue, (double)chroma, (double)tone};
+    return "#" + QString::fromStdString(
+                     material_color_utilities::HexFromArgb(d.ToInt()))
+                     .remove(0, 2);
+  }
+
+ signals:
+  void hChanged();
+  void cChanged();
+  void tChanged();
+  void rgbChanged();
+
+ private:
+  int hue;
+  int chroma;
+  int tone;
+};
+
+int main(int argc, char** argv) {
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+
+  qmlRegisterType<Hct>("Hct", 1, 0, "Hct");
+
+  const QString mainQmlFile = "/Main.qml";
+
+  const QUrl url(QStringLiteral("qrc:") + mainQmlFile);
+
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject* obj, const QUrl& objUrl) {
+        if (!obj && url == objUrl) QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
+
+  return app.exec();
+}
+
+#include "main.moc"
